d71bd2f1a8d40c25f38ace9af161b2c3d2a00b83
Sabbeh Hassan <hassansubbeh2446@gmail.com>
2025-06-10 06:04:04 +0500

    create Popular Destination component and import in Home page

---
+++ src/data/popularDestinations.js
@@ -0,0 +1,54 @@
+// src/data/popularDestinations.js
+import hunza from "../assets/Destinations/Hunza.jpg";
+import skardu from "../assets/Destinations/Skardu.jpg";
+import fairyMeadows from "../assets/Destinations/FairyMedows.jpg";
+import khunjerab from "../assets/Destinations/KhunjrabPass.jpg";
+import attabad from "../assets/Destinations/Attabad.jpg";
+import deosai from "../assets/Destinations/DeosaiPlains.jpg";
+
+const popularDestinations = [
+  {
+    id: 1,
+    name: "Hunza Valley",
+    image: hunza,
+    description:
+      "A paradise on Earth with majestic mountains and vibrant culture.",
+  },
+  {
+    id: 2,
+    name: "Skardu",
+    image: skardu,
+    description:
+      "Gateway to some of the world’s highest peaks and scenic lakes.",
+  },
+  {
+    id: 3,
+    name: "Fairy Meadows",
+    image: fairyMeadows,
+    description:
+      "Stunning green plains with breathtaking views of Nanga Parbat.",
+  },
+  {
+    id: 4,
+    name: "Khunjerab Pass",
+    image: khunjerab,
+    description:
+      "High-altitude pass connecting Pakistan to China via the Karakoram Highway.",
+  },
+  {
+    id: 5,
+    name: "Attabad Lake",
+    image: attabad,
+    description:
+      "Crystal blue lake formed by a natural disaster, now a tourist gem.",
+  },
+  {
+    id: 6,
+    name: "Deosai Plains",
+    image: deosai,
+    description:
+      "The second highest plateau in the world, home to diverse wildlife.",
+  },
+];
+
+export default popularDestinations;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../../components/heroSection";
+import PopularDestinations from "../../components/PopularDestinations";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <PopularDestinations />
       {/* Add Intro, Destinations, Contact later */}
     </>
   );
