5eae4680a9406493004209e5328b92ad3cf857d1
Sabbeh Hassan <hassansubbeh2446@gmail.com>
2025-06-10 07:48:49 +0500

    testominals component is created and added in home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "../../components/heroSection";
 import PopularDestinations from "../../components/PopularDestinations";
 import TourPackages from "../../components/TourPackages";
+import Testimonials from "../../components/Testimonials";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <PopularDestinations />
       <TourPackages />
+      <Testimonials />
       {/* Add Intro, Destinations, Contact later */}
     </>
   );
